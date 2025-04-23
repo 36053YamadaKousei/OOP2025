cfc770bf0b298bed369867fce568d36769f8be52
YamadaKousei <oibc36053@ojs.ac.jp>
2025-04-23 10:22:42 +0900

    インチからメートル変換完成

---
+++ Exercise02/InchConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02{
+    class InchConverter{
+
+        //定数
+        private const double raito = 39.37;
+
+        //インチからメートルを求める
+        public static double FromMeter(double meter) {
+            return meter / raito;
+        }
+    }
+}
---
+++ Exercise02/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            PrintInchToMeterList(1, 10);
+        }
+        static void PrintInchToMeterList(int start,int end) {
+            //インチからメートルへの対応表を出力
+            for (int inch = start; inch <= end; inch++) {
+                double meter = InchConverter.FromMeter(inch);
+                Console.WriteLine($"{inch}inch = {meter:0.0000}m");
+            }
+        }
+    }
+}
+
