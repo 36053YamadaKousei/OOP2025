08f0c783acfcc34ba6aba74d9eae08c149edab6a
YamadaKousei <oibc36053@ojs.ac.jp>
2025-05-13 10:37:18 +0900

    2.1.3まで

---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01{
+    //2.1.1
+    public class Song{
+        //歌のタイトル
+        public string Title { get; private set; }
+        //アーティスト名
+        public string ArtistName { get; private set; }
+        //演奏時間（秒）
+        public int Length { get; private set; }
+
+        //2.1.2
+        public Song (string title, string artistName, int length) {
+            this.Title = title;
+            this.ArtistName = artistName;
+            this.Length = length;
+        }
+    }
+}
