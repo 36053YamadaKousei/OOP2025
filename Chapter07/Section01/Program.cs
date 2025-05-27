83fa6e7e851be1e454928f9284fad77841155630
YamadaKousei <oibc36053@ojs.ac.jp>
2025-05-27 12:10:38 +0900

    Bookまで

---
+++ Chapter07/Section01/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var books = Books.GetBooks();
+
+            //①本の平均金額を表示
+            Console.WriteLine((int)books.Average(x => x.Price));
+
+            //②本のページ合計を表示
+            Console.WriteLine(books.Sum(x => x.Pages));
+
+            //③金額の安い書籍名と金額を表示
+
+            Console.WriteLine(books.Min(x => x.Price));
+
+            //④ページが多い書籍名とページ数を表示
+            Console.WriteLine(books.Max(x => x.Pages));
+
+            //⑤タイトルに「物語」が含まれている書籍名をすべて表示
+
+
+        }
+    }
+}
