c01e2f33bf567d2477ec84fc64535e4509f3de63
YamadaKousei <oibc36053@ojs.ac.jp>
2025-05-28 12:10:40 +0900

    7.2.7まで

---
+++ Chapter07/Section05/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Section05 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "The quick brown fox jumps over the lazy dog";
+            var words = text.Split(' ');
+            var word = words.Where(s => s.Length == 4);
+
+            
+        }
+    }
+}
---
+++ Chapter07/演習問題/Exercise02/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace Exercise02 {
+
+    public class Book {
+        public string Title { get; init; } = string.Empty;
+        public int Price { get; init; }
+        public int Pages { get; init; }
+    }
+}
