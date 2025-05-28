6b67c0dd3ea4d2bf655ba627e001785df5c0c5a3
YamadaKousei <oibc36053@ojs.ac.jp>
2025-05-28 10:02:16 +0900

    Section01完成

--- Chapter07/Section01/Program.cs
+++ Chapter07/Section01/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Section01 {
+﻿using System;
+
+namespace Section01 {
     internal class Program {
         static void Main(string[] args) {
             var books = Books.GetBooks();
@@ -7,18 +9,25 @@
             Console.WriteLine((int)books.Average(x => x.Price));
 
             //②本のページ合計を表示
-            Console.WriteLine(books.Sum(x => x.Pages));
+            Console.WriteLine((int)books.Sum(x => x.Pages));
 
             //③金額の安い書籍名と金額を表示
-
-            Console.WriteLine(books.Min(x => x.Price));
+            var book = books.Where(x => x.Price == books.Min(b => b.Price));
+            foreach (var item in book) {
+                Console.WriteLine(item.Title + ":" + item.Price);
+            }
 
             //④ページが多い書籍名とページ数を表示
-            Console.WriteLine(books.Max(x => x.Pages));
+            var page = books.Where(x => x.Pages == books.Max(b => b.Pages));
+            foreach (var item in page) {
+                Console.WriteLine(item.Title + ":" + item.Pages);
+            }
 
             //⑤タイトルに「物語」が含まれている書籍名をすべて表示
-
-
+            var titles = books.Where(x => x.Title.Contains("物語"));
+            foreach (var item in titles) {
+                Console.WriteLine(item.Title);
+            }
         }
     }
 }
