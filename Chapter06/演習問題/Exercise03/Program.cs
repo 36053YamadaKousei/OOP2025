3e6575f979a6f6ea03cbe71265d2fe83703393b7
YamadaKousei <oibc36053@ojs.ac.jp>
2025-06-04 11:29:06 +0900

    6.3.99完成

--- Chapter06/演習問題/Exercise03/Program.cs
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -21,6 +21,40 @@ namespace Exercise03 {
             Console.WriteLine("6.3.5");
             Exercise5(text);
 
+            Console.WriteLine("6.3.99");
+            Exercise6(text);
+        }
+
+        private static void Exercise6(string text) {
+            Console.WriteLine("出現アルファベットの個数");
+            var str = text.ToLower().Replace(" ","");
+
+            //辞書で集計
+            var alphDicCount = Enumerable.Range('a', 26)
+                .ToDictionary(num => ((char)num).ToString(), num => 0);
+
+            foreach (var alph in str) {
+                alphDicCount[alph.ToString()]++;
+            }
+            foreach (var item in alphDicCount) {
+                Console.WriteLine($"{item.Key}:{item.Value}");
+            }
+            Console.WriteLine();
+
+            //配列で集計
+            var array = Enumerable.Repeat(0, 26).ToArray();
+            foreach(var alph in str) {
+                array[alph - 'a']++;
+            }
+            for (char ch = 'a'; ch < 'z'; ch++) {
+                Console.WriteLine($"{ch}:{array[ch - 'a']}");
+            }
+            Console.WriteLine();
+
+            //'a'から順にカウントして出力
+            for (char ch = 'a';ch <= 'z'; ch++) {
+                Console.WriteLine($"{ch}:{text.Count(tc => tc == ch)}");
+            }
         }
 
         private static void Exercise1(string text) {
