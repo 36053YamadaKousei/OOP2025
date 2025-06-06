9d11860da3b4275220cfa0b6e4b7aadba8d5f955
YamadaKousei <oibc36053@ojs.ac.jp>
2025-06-06 14:26:45 +0900

    レコード型

--- Chapter05/Section02/Program.cs
+++ Chapter05/Section02/Program.cs
@@ -5,21 +5,9 @@
             var appVer2 = new AppVersion(5, 1);
 
             Console.WriteLine(appVer1);
-            if (appVer1 == appVer2) {
-                Console.WriteLine("等しい");
-            } else {
-                Console.WriteLine("等しくない");
-            }
+
         }
     }
-    public record AppVersion(int m, int mi, int b = 0, int r = 0) {
-        public int Major { get; init; } = m;
-        public int Minor { get; init; } = mi;
-        public int Build { get; init; } = b;
-        public int Revision { get; init; } = r;
-
-        public override string ToString() =>
-            $"{Major}.{Minor}.{Build}.{Revision}";
-
+    public record AppVersion(int Major, int Minor, int Build = 0, int Revision = 0) {
     }
 }
