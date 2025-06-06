3be009c12fc80994a6ca14638ee8b254babbb797
YamadaKousei <oibc36053@ojs.ac.jp>
2025-06-06 13:57:15 +0900

    オプション引数

---
+++ Chapter05/Section01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Immutable;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var obj = new MySample();
+            obj.MyList.Add(6).RemoveAt(0);
+            obj.MyList.ForEach(new=>Console.Write($"{n} "));
+            Console.WriteLine();
+            newList.ForEach(new=>Console.Write($"{n} "));
+        }
+    }
+    class MySample {
+        public ImmutableList<int> MyList { get; private set; }
+
+        public MySample() {
+            var list = new List<int>() { 1, 2, 3, 4, 5 };
+            MyList = list.ToImmutableList();
+        }
+    }
+    public class Person {
+        public string GivenName { get; private set; }
+
+        public string FamilyName { get; private set; }
+
+        public Person(string familyName, string givenName) {
+            FamilyName = familyName;
+            GivenName = givenName;
+        }
+
+        // メソッド内でプロパティの値を変更できる。
+        public void ChangeFamilyName(string name) => FamilyName = name;
+
+
+    }
+}
---
+++ Chapter05/Section02/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Section02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var appVer = new AppVersion(5, 1, 3,125);
+            Console.WriteLine(appVer);
+        }
+    }
+    public class AppVersion {
+        public int Major { get; init; }
+        public int Minor { get; init; }
+        public int Build { get; init; }
+        public int Revision { get; init; }
+
+        public AppVersion(int major, int minor, int build = 0, int revision = 0) {
+            Major = major;
+            Minor = minor;
+            Build = build;
+            Revision = revision;
+        }
+
+        public override string ToString() =>
+            $"{Major}.{Minor}.{Build}.{Revision}";
+
+    }
+}
