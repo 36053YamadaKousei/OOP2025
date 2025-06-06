a12820ee9215982e3d4e8df503b7d0c9324287ba
YamadaKousei <oibc36053@ojs.ac.jp>
2025-06-06 14:12:06 +0900

    プライマリコンストラクター

--- Chapter05/Section02/Program.cs
+++ Chapter05/Section02/Program.cs
@@ -1,22 +1,15 @@
 ﻿namespace Section02 {
     internal class Program {
         static void Main(string[] args) {
-            var appVer = new AppVersion(5, 1, 3,125);
+            var appVer = new AppVersion(5, 1, 3);
             Console.WriteLine(appVer);
         }
     }
-    public class AppVersion {
-        public int Major { get; init; }
-        public int Minor { get; init; }
-        public int Build { get; init; }
-        public int Revision { get; init; }
-
-        public AppVersion(int major, int minor, int build = 0, int revision = 0) {
-            Major = major;
-            Minor = minor;
-            Build = build;
-            Revision = revision;
-        }
+    public class AppVersion(int m, int mi, int b = 0, int r = 0) {
+        public int Major { get; init; } = m;
+        public int Minor { get; init; } = mi;
+        public int Build { get; init; } = b;
+        public int Revision { get; init; } = r;
 
         public override string ToString() =>
             $"{Major}.{Minor}.{Build}.{Revision}";
