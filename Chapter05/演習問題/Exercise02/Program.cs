c4bfe05d8a15be538c607bff2e6832133d11d9da
YamadaKousei <oibc36053@ojs.ac.jp>
2025-06-10 11:38:39 +0900

    5.2.4null合体演算子

--- Chapter05/演習問題/Exercise02/Program.cs
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -1,7 +1,59 @@
-﻿namespace Exercise02 {
+﻿using Exercise01;
+
+namespace Exercise02 {
     public class Program {
         static void Main(string[] args) {
-            Exercise01 YearMonth;
+            // 5.2.1
+            var ymCollection = new YearMonth[] {
+                new YearMonth(1980, 1),
+                new YearMonth(1990, 4),
+                new YearMonth(2000, 7),
+                new YearMonth(2010, 9),
+                new YearMonth(2024, 12),
+            };
+
+            Console.WriteLine("5.2.2");
+            Exercise2(ymCollection);
+
+            Console.WriteLine("5.2.4");
+            Exercise4(ymCollection);
+
+            Console.WriteLine("5.2.5");
+            Exercise5(ymCollection);
+
+        }
+        //5.2.2
+        private static void Exercise2(YearMonth[] ymCollection) {
+
+            foreach (var ym in ymCollection) {
+                Console.WriteLine(ym);
+            }
+        }
+        //5.2.3
+        //メソッド作成「メソッド名：FindFirst21c」
+        public static YearMonth? FindFirst21c(YearMonth[] ymCollection) {
+            foreach (var ym in ymCollection) {
+                if (ym.Is21Century)
+                    return ym;
+            }
+            return null;
+        }
+
+        //5.2.4
+        private static void Exercise4(YearMonth[] ymCollection) {
+            //var ym = FindFirst21c(ymCollection);
+            //if (ym is null) {
+            //    Console.WriteLine("21世紀のデータはありません");
+            //} else {
+            //    Console.WriteLine(ym);
+            //}
+            
+            //null合体演算子
+            Console.WriteLine(FindFirst21c(ymCollection)?.ToString() ?? "21世紀のデータはありません");
+        }
+        //5.2.5
+        private static void Exercise5(YearMonth[] ymCollection) {
+            
         }
     }
 }
