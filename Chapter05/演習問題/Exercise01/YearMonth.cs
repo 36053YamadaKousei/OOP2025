dc113cd77f6b4482f112718e39009ea6bc05f9f3
YamadaKousei <oibc36053@ojs.ac.jp>
2025-06-10 11:58:40 +0900

    5.3完成

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -6,14 +6,9 @@ using System.Threading.Tasks;
 
 namespace Exercise01{
     //5.1.1
-    public class YearMonth{
-        public int Year { get; init; }
-        public int Month { get; init; }
+    public record YearMonth(int Year, int Month) {
+        
 
-        public YearMonth(int year, int month) {
-            Year = year;
-            Month = month;
-        }
         //5.1.2
         //設定されている西暦が２１世紀か判定する
         //Yearが2001～2100年の間ならtrue、それ以外ならfalseを返す
