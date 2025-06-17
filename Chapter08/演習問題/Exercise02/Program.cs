05ef56fe964dc117dad011842fd5e6171ad14c49
YamadaKousei <oibc36053@ojs.ac.jp>
2025-06-17 10:52:27 +0900

    8.2

--- Chapter08/演習問題/Exercise02/Program.cs
+++ Chapter08/演習問題/Exercise02/Program.cs
@@ -1,7 +1,31 @@
 ﻿namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+
+            var abbrs = new Abbreviations();
+
+            abbrs.Add("IOC", "国際オリンピック委員会");
+            abbrs.Add("NPT", "核兵器不拡散条約");
+
+
+            var count = abbrs.Count;
+            Console.WriteLine(abbrs.Count);
+            Console.WriteLine();
+
+
+            if (abbrs.Remove("NPI")) {
+                Console.WriteLine(abbrs.Count);
+            }
+
+            if (abbrs.Remove("NPI")) {
+                Console.WriteLine("削除できません");
+            }
+            Console.WriteLine();
+
+            //8.2.4
+            var query = abbrs.Where(x => x.Key.Length == 3);
+
+
         }
     }
 }
