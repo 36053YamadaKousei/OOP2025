653fce7c96c2f11e1147c7f8ad8fe62a8ee6e9ca
YamadaKousei <oibc36053@ojs.ac.jp>
2025-06-17 12:06:01 +0900

    設問１_提出

--- Test01/Test01/ScoreCounter.cs
+++ Test01/Test01/ScoreCounter.cs
@@ -4,12 +4,12 @@
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
-            
+            _score = (IEnumerable<Student>?)ReadScore(filePath);
         }
 
         //メソッドの概要： 
         private static Dictionary<string,int> ReadScore(string filePath) {
-           
+            
 
 
 
@@ -17,11 +17,15 @@
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
-            
-
-
-
-
+            IDictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (Student score in _score) {
+                if (dict.ContainsKey(score.Name)) {
+                    dict[score.Name] += score.Score;
+                } else {
+                    dict[score.Name] = score.Score;
+                }
+            }
+            return dict;
         }
     }
 }
