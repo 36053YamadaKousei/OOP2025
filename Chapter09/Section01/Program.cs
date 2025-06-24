df53803aaa27d151c56b2948cd6a367556a6cf78
YamadaKousei <oibc36053@ojs.ac.jp>
2025-06-24 14:52:31 +0900

    曜日を漢字で

--- Chapter09/Section01/Program.cs
+++ Chapter09/Section01/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Section01 {
+﻿using System.Globalization;
+
+namespace Section01 {
     internal class Program {
         static void Main(string[] args) {
             var today = new DateTime(2025,7,12);//日付
@@ -9,10 +11,28 @@
             Console.WriteLine($"Now:{now}");
 
             //①自分の生年月日は何曜日をプログラムを書いて調べる
-            var birthday = new DateTime(2005,11,2);
-            DayOfWeek dayOfWeek = birthday.DayOfWeek;
+            //var dateOfBirth = new DateTime(2005,11,2);
+            //DayOfWeek dayOfWeek = dateOfBirth.DayOfWeek;
+            //Console.WriteLine(dayOfWeek);
 
-            Console.WriteLine(dayOfWeek);
+            Console.Write("西暦：");
+            var year = int.Parse(Console.ReadLine());
+            Console.Write("月：");
+            var month = int.Parse(Console.ReadLine());
+            Console.Write("日：");
+            var day = int.Parse(Console.ReadLine());
+
+            //平成○○年○月○日は○曜日です　←曜日は漢字で（P202）
+            var date = new DateTime(year,month,day);
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            var str = date.ToString("ggyy年M月d日", culture);
+            var dayOfWeek = culture.DateTimeFormat.GetDayName(date.DayOfWeek);
+            Console.WriteLine($"{str}は{dayOfWeek}");
+            
+
+            //②うるう年の判定プログラムを作成する
+            
         }
     }
 }
