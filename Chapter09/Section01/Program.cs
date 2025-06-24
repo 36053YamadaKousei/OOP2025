fa35ded64f6930e799c10afe6d303089ef8070a2
YamadaKousei <oibc36053@ojs.ac.jp>
2025-06-24 13:46:23 +0900

    生年月日は何曜日か

---
+++ Chapter09/Section01/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var today = new DateTime(2025,7,12);//日付
+            var now = DateTime.Now;
+
+            
+            Console.WriteLine($"Today:{today.Month}");
+            Console.WriteLine($"Now:{now}");
+
+            //①自分の生年月日は何曜日をプログラムを書いて調べる
+            var birthday = new DateTime(2005,11,2);
+            DayOfWeek dayOfWeek = birthday.DayOfWeek;
+
+            Console.WriteLine(dayOfWeek);
+        }
+    }
+}
