b31eb0721ce3189d89b8c5c8b3b9fa61e82b49d3
YamadaKousei <oibc36053@ojs.ac.jp>
2025-07-17 16:52:18 +0900

    追追試提出

---
+++ Test202503/Test01/ScoreCounter.cs
@@ -0,0 +1,42 @@
+﻿using static System.Formats.Asn1.AsnWriter;
+
+namespace Test01 {
+    public class ScoreCounter {
+        private IEnumerable<Student> _score;
+
+        // コンストラクタ
+        public ScoreCounter(string filePath) {
+
+            _score = ReadScore(filePath);
+        }
+
+        //メソッドの概要：生徒のスコアを読み込み、Studentオブジェクトのリストを返す
+        private static IEnumerable<Student> ReadScore(string filePath) {
+            var sales = new List<Student>();
+            var lines = File.ReadAllLines(filePath);
+            foreach (var line in lines) {
+                var items = line.Split(',');
+                Student student = new Student() {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                sales.Add(student);
+            }
+            return sales;
+        }
+
+        //メソッドの概要：生徒ごとにスコアを求める
+        public IDictionary<string, int> GetPerStudentScore() {
+            var dict = new Dictionary<string, int>();
+            foreach (var score in _score) {
+                if (dict.ContainsKey(score.Name)) {
+                    dict[score.Name] += score.Score;
+                } else {
+                    dict[score.Name] = score.Score;
+                }
+            }
+            return dict;
+        }
+    }
+}
