2a47b5a0c04a9122e5dfa27a78816b07c6d604df
YamadaKousei <oibc36053@ojs.ac.jp>
2025-07-22 15:54:46 +0900

    コンボボックスへURL返却

--- FromApps/RSSreader/Form1.Designer.cs
+++ FromApps/RSSreader/Form1.Designer.cs
@@ -23,29 +23,22 @@
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            tbUrl = new TextBox();
             btRssGet = new Button();
             lbTitles = new ListBox();
             wvRssLink = new Microsoft.Web.WebView2.WinForms.WebView2();
             btGoBack = new Button();
             btGoForward = new Button();
+            label1 = new Label();
+            cbUrl = new ComboBox();
             ((System.ComponentModel.ISupportInitialize)wvRssLink).BeginInit();
             SuspendLayout();
-            // 
-            // tbUrl
-            // 
-            tbUrl.Font = new Font("Yu Gothic UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 128);
-            tbUrl.Location = new Point(116, 12);
-            tbUrl.Name = "tbUrl";
-            tbUrl.Size = new Size(604, 33);
-            tbUrl.TabIndex = 0;
             // 
             // btRssGet
             // 
             btRssGet.Font = new Font("Yu Gothic UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 128);
-            btRssGet.Location = new Point(726, 12);
+            btRssGet.Location = new Point(727, 5);
             btRssGet.Name = "btRssGet";
-            btRssGet.Size = new Size(94, 33);
+            btRssGet.Size = new Size(98, 33);
             btRssGet.TabIndex = 1;
             btRssGet.Text = "取得";
             btRssGet.UseVisualStyleBackColor = true;
@@ -57,9 +50,9 @@
             lbTitles.Font = new Font("Yu Gothic UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 128);
             lbTitles.FormattingEnabled = true;
             lbTitles.ItemHeight = 21;
-            lbTitles.Location = new Point(12, 54);
+            lbTitles.Location = new Point(12, 139);
             lbTitles.Name = "lbTitles";
-            lbTitles.Size = new Size(808, 130);
+            lbTitles.Size = new Size(813, 172);
             lbTitles.TabIndex = 2;
             lbTitles.Click += lbTitles_Click;
             // 
@@ -68,9 +61,9 @@
             wvRssLink.AllowExternalDrop = true;
             wvRssLink.CreationProperties = null;
             wvRssLink.DefaultBackgroundColor = Color.White;
-            wvRssLink.Location = new Point(12, 190);
+            wvRssLink.Location = new Point(12, 317);
             wvRssLink.Name = "wvRssLink";
-            wvRssLink.Size = new Size(808, 465);
+            wvRssLink.Size = new Size(813, 411);
             wvRssLink.TabIndex = 3;
             wvRssLink.ZoomFactor = 1D;
             wvRssLink.SourceChanged += wvRssLink_SourceChanged;
@@ -78,7 +71,7 @@
             // btGoBack
             // 
             btGoBack.Font = new Font("Yu Gothic UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 128);
-            btGoBack.Location = new Point(12, 12);
+            btGoBack.Location = new Point(12, 7);
             btGoBack.Name = "btGoBack";
             btGoBack.Size = new Size(45, 33);
             btGoBack.TabIndex = 4;
@@ -89,7 +82,7 @@
             // btGoForward
             // 
             btGoForward.Font = new Font("Yu Gothic UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 128);
-            btGoForward.Location = new Point(63, 12);
+            btGoForward.Location = new Point(63, 7);
             btGoForward.Name = "btGoForward";
             btGoForward.Size = new Size(47, 33);
             btGoForward.TabIndex = 5;
@@ -97,17 +90,37 @@
             btGoForward.UseVisualStyleBackColor = true;
             btGoForward.Click += btGoForward_Click;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Yu Gothic UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            label1.Location = new Point(12, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(199, 25);
+            label1.TabIndex = 6;
+            label1.Text = "URL又はお気に入り名称";
+            // 
+            // cbUrl
+            // 
+            cbUrl.Font = new Font("Yu Gothic UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 128);
+            cbUrl.FormattingEnabled = true;
+            cbUrl.Location = new Point(217, 44);
+            cbUrl.Name = "cbUrl";
+            cbUrl.Size = new Size(608, 33);
+            cbUrl.TabIndex = 7;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(832, 667);
+            ClientSize = new Size(837, 740);
+            Controls.Add(cbUrl);
+            Controls.Add(label1);
             Controls.Add(btGoForward);
             Controls.Add(btGoBack);
             Controls.Add(wvRssLink);
             Controls.Add(lbTitles);
             Controls.Add(btRssGet);
-            Controls.Add(tbUrl);
             Name = "Form1";
             Text = "RSSリーダー";
             Load += Form1_Load;
@@ -117,12 +130,12 @@
         }
 
         #endregion
-
-        private TextBox tbUrl;
         private Button btRssGet;
         private ListBox lbTitles;
         private Microsoft.Web.WebView2.WinForms.WebView2 wvRssLink;
         private Button btGoBack;
         private Button btGoForward;
+        private Label label1;
+        private ComboBox cbUrl;
     }
 }
--- FromApps/RSSreader/Form1.cs
+++ FromApps/RSSreader/Form1.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
@@ -6,6 +7,21 @@ namespace RssReader {
     public partial class Form1 : Form {
 
         private List<ItemData> items;
+
+        Dictionary<string, string> rssUrlDict = new Dictionary<string, string>() {
+            {"主要","https://news.yahoo.co.jp/rss/topics/top-picks.xml"},
+            {"国内","https://news.yahoo.co.jp/rss/topics/domestic.xml"},
+            {"国際","https://news.yahoo.co.jp/rss/topics/world.xml"},
+            {"経済","https://news.yahoo.co.jp/rss/topics/business.xml"},
+            {"エンタメ","https://news.yahoo.co.jp/rss/topics/entertainment.xml"},
+            {"スポーツ","https://news.yahoo.co.jp/rss/topics/sports.xml"},
+            {"IT","https://news.yahoo.co.jp/rss/topics/it.xml"},
+            {"科学","https://news.yahoo.co.jp/rss/topics/science.xml"},
+            {"地域","https://news.yahoo.co.jp/rss/topics/local.xml"}
+        };
+
+        
+
 
         public Form1() {
             InitializeComponent();
@@ -15,7 +31,8 @@ namespace RssReader {
 
             using (var hc = new HttpClient()) {
 
-                string xml = await hc.GetStringAsync(tbUrl.Text);
+
+                string xml = await hc.GetStringAsync(getRssUrl(cbUrl.Text));
                 XDocument xdoc = XDocument.Parse(xml);
 
                 //RSSを解析して必要な要素を取得
@@ -32,25 +49,25 @@ namespace RssReader {
 
             }
         }
+        //コンボボックスの文字列確認してURL返却
+        private string getRssUrl(string str) {
+            if (rssUrlDict.ContainsKey(str)) {
+                return rssUrlDict[str];
+            }
+            return str;
+        }
 
         private void lbTitles_Click(object sender, EventArgs e) {
             wvRssLink.Source = new Uri(items[lbTitles.SelectedIndex].Link);
         }
 
-
-
-
         //戻るボタン
         private void btGoBack_Click(object sender, EventArgs e) {
-
             wvRssLink.GoBack();
-
         }
         //進むボタン
         private void btGoForward_Click(object sender, EventArgs e) {
-
             wvRssLink.GoForward();
-
         }
 
         private void wvRssLink_SourceChanged(object sender, Microsoft.Web.WebView2.Core.CoreWebView2SourceChangedEventArgs e) {
@@ -59,6 +76,8 @@ namespace RssReader {
         }
 
         private void Form1_Load(object sender, EventArgs e) {
+            cbUrl.DataSource = rssUrlDict.Select(k => k.Key).ToList();
+
             
         }
     }
