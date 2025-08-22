9f66ad10a896f86f142dd34661891d269da69693
YamadaKousei <oibc36053@ojs.ac.jp>
2025-08-22 16:00:12 +0900

    正規表現 演習問題11.3

---
+++ Chapter11/Section02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Section02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var strings = new[] {
+                "Microsofft Windows",
+                "windows",
+                "Windows Server",
+                "Windows"
+            };
+
+            var regex = new Regex(@"^(W|w)indows$");
+
+            //パターンと完全一致している文字列の個数をカウント
+            var count = strings.Count(s => regex.IsMatch(s));
+            Console.WriteLine($"{count}行と一致");
+
+            //パターンと完全一致している文字列を出力する
+            //var datas = strings.Where(s => regex.IsMatch(s));
+            //foreach (var item in datas) {
+            //    Console.WriteLine(item);
+            //}
+
+            strings.Where(s => regex.IsMatch(s)).ToList().ForEach(Console.WriteLine);
+
+        }
+    }
+}
