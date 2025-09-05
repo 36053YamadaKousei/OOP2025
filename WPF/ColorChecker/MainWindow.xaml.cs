8ee44368aaa48ce63529b7e397c36df9f505d8a0
YamadaKousei <oibc36053@ojs.ac.jp>
2025-09-05 15:32:02 +0900

    スライダーで背景色変更

---
+++ WPF/ColorChecker/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ColorChecker
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //すべてのスライダーから呼ばれるイベントハンドラ
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            //colorAreaの背景色はスライダーで指定されたRGBの色を表示
+            byte r = (byte)rSlider.Value;
+            byte g = (byte)gSlider.Value;
+            byte b = (byte)bSlider.Value;
+
+            Color color = Color.FromRgb(r, g, b);
+            Brush brush = new SolidColorBrush(color);
+            colorArea.Background = brush;
+        }
+    }
+}
