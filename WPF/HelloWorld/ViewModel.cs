57fe13d1c4945cb295e908569eb851345380d838
YamadaKousei <oibc36053@ojs.ac.jp>
2025-10-02 14:30:41 +0900

    InotifyPropertyChangedの場合

---
+++ WPF/HelloWorld/ViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld{
+    class ViewModel : INotifyPropertyChanged{
+
+        public ViewModel() {
+            ChangeMesageCommand = new DelegateCommand(
+                () => GreetingMesage = "Bye-bye world");
+        }
+
+        private string _greetingMesage = "Hello World!";
+        public string GreetingMesage {
+            get => _greetingMesage;
+            set {
+                if (_greetingMesage != value) {
+                    _greetingMesage = value;
+                    PropertyChanged?.Invoke(
+                        this, new PropertyChangedEventArgs(nameof(GreetingMesage)));
+                }
+            }
+        }
+
+        public DelegateCommand ChangeMesageCommand { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
