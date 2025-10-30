53a29a6256ff2a713930298440c6752cfdca898b
YamadaKousei <oibc36053@ojs.ac.jp>
2025-10-30 14:59:14 +0900

    コード14.7

--- Chapter14/Section03/Form1.cs
+++ Chapter14/Section03/Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace Section03 {
     public partial class Form1 : Form {
         public Form1() {
@@ -6,12 +8,17 @@ namespace Section03 {
 
         private async Task button1_Click(object sender, EventArgs e) {
             toolStripStatusLabel1.Text = "";
-            await Task.Run(()=> DoLongTimeWork());
+            var elapsed = await Task.Run(()=> DoLongTimeWork());
             toolStripStatusLabel1.Text = "èIóπ";
         }
 
-        private void DoLongTimeWork() {
+        private long DoLongTimeWork() {
+            var sw = Stopwatch.StartNew();
+
             System.Threading.Thread.Sleep(5000);
+            sw.Stop();
+            return sw.ElapsedMilliseconds;
         }
+
     }
 }
