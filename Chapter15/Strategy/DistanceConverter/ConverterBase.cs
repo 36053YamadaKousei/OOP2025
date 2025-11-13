3f2d274a81cae84f411d9db28dcd2530954bf081
YamadaKousei <oibc36053@ojs.ac.jp>
2025-11-13 15:55:59 +0900

    strategyパターン

---
+++ Chapter15/Strategy/DistanceConverter/ConverterBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+    public abstract class ConverterBase {
+        // nameで与えられた
+        public abstract bool IsMyUnit(string name);
+        //メートルとの比率(この比率をかけるとメートルに変換できる)
+        protected abstract double Raito { get; }
+        //距離の単位名(例えば"メートル"、"フィート"など)
+        public abstract string UnitName { get; }
+        //メートルからの変換
+        public double FromMeter(double meter) => meter / Raito;
+        //メートルへの変換
+        public double ToMeter(double feet) => feet * Raito;
+
+    }
+}
---
+++ Chapter15/Strategy/DistanceConverter/ConverterFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+    internal class ConverterFactory {
+        //あらかじめインスタンスを生成し配列に入れておく
+        private readonly static ConverterBase[] _converters = {
+            new MeterConverter(),
+            new FeetConverter(),
+            new InchConverter(),
+            new YardConverter()
+        };
+        public static ConverterBase? GetInstance(string name) =>
+            _converters.FirstOrDefault(x => x.IsMyUnit(name));
+    }
+}
