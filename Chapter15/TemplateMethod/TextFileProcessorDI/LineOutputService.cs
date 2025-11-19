c97735b3330a566dfdb0f94c1c89336a4baa645b
YamadaKousei <oibc36053@ojs.ac.jp>
2025-11-19 10:33:34 +0900

    問題15.1完成

---
+++ Chapter15/TemplateMethod/TextFileProcessorDI/LineOutputService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileProcessorDI {
+    //問題15.3
+    public class LineOutputService : ITextFileService {
+        int _linecount;
+
+        public void Initialize(string fname) {
+            _linecount = 0;
+        }
+
+        public void Execute(string line) {
+            if (_linecount < 20) {
+                Console.WriteLine(line);
+            }
+            _linecount++;
+        }
+
+        public void Terminate() {
+            Console.WriteLine("出力終了");
+        }
+    }
+}
--- Chapter15/TemplateMethod/TextFileProcessorDI/Program.cs
+++ Chapter15/TemplateMethod/TextFileProcessorDI/Program.cs
@@ -1,7 +1,8 @@
 ﻿namespace TextFileProcessorDI {
     internal class Program {
         static void Main(string[] args) {
-            var service = new LineCountorServise();
+
+            var service = new LineToHalfNumberService();
             var processor = new TextFileProcessor(service);
             Console.Write("パスの入力：");
             processor.Run(Console.ReadLine());
