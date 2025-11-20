c2c194fa573d3b4df08663053b6ab2687bd12b17
YamadaKousei <oibc36053@ojs.ac.jp>
2025-11-20 13:15:45 +0900

    TenkiApp新規作成

---
+++ WebAPI/WeatherApp/WeatherResponce.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp {
+    public class WeatherResponce {
+        public Current current { get; set; }
+    }
+}
